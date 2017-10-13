5478533aa9935a900ce11b3cb6e09e4e76c73fe1
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-10-13 20:27:23 +0300

    Update Лабораторная1.c

--- Лабораторная1.c
+++ Лабораторная1.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <math.h>
+#include <stlib.h>
+
 int main(){
+ 
 char a;
 float b;
+ 
 scanf("%f%c",&b,&a);
+ 
 switch(a)
-{case'C':printf("F=%.2f""\nK=%.2f",(b*1.8+32),b+273.15);
+ 
+{case'C':case'c':
+ 
+ printf("F=%.2f""\nK=%.2f",(b*1.8+32),b+273.15);
 break;
- case'F':printf("C=%.2f""\nK=%.2f",(b-32)/1.8,((b-32)/1.8)+273.15);
+ 
+ case'F':case'f':
+ 
+ printf("C=%.2f""\nK=%.2f",(b-32)/1.8,((b-32)/1.8)+273.15);
  break;
- case'K':printf("C=%.2f""\nF=%.2f",(b-273.15),((b-273.15)*1.8)+32);
+ 
+ case'K':case'k':
+ 
+ printf("C=%.2f""\nF=%.2f",(b-273.15),((b-273.15)*1.8)+32);
  break;
- default:printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
+ 
+ default:
+ 
+ printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
          printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
          printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
   break;
