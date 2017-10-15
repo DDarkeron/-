c676480b6e9b1d4be84f90410d5436de410f0cbd
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-10-15 16:36:17 +0300

    Update лабораторная1.1.c

--- лабораторная1.1.c
+++ лабораторная1.1.c
@@ -4,27 +4,23 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-
- char a;
+char a;
 float b;
-
- b=atof(argv[2]);
- 
+ b=atof(argv[1]);
+ a=argv[2];
  if(argc==1){
     printf("Enter number and first letter of temperature");
  }
  else
 
 
-
-switch(a)
-
-{case'C':case'c':
+switch(a){
+case'C':case'c':
 
  printf("F=%.2f""\nK=%.2f",(b*1.8+32),b+273.15);
 break;
 
- case'F':case'f':
+case'F':case'f':
 
  printf("C=%.2f""\nK=%.2f",(b-32)/1.8,((b-32)/1.8)+273.15);
  break;
@@ -32,14 +28,14 @@ break;
  case'K':case'k':
 
  printf("C=%.2f""\nF=%.2f",(b-273.15),((b-273.15)*1.8)+32);
+break;
+
+default:
+
+    printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
+    printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
+    printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
  break;
-
- default:
-
- printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
-         printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
-         printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
-  break;
 }
 
 
