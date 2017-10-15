7e32a74d62462395e5b3d04036fe1b6c14fa2842
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-10-15 16:45:49 +0300

    Update лабораторная1.1.c

--- лабораторная1.1.c
+++ лабораторная1.1.c
@@ -12,22 +12,32 @@ float b;
     printf("Enter number and first letter of temperature");
  }
  else
-
-
+ if (argc==2){
+    printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
+    printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
+    printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
+ }
+else{
 switch(a){
 case'C':case'c':
-
+if (b>-273)
  printf("F=%.2f""\nK=%.2f",(b*1.8+32),b+273.15);
+ else
+    printf("temperature doesnt exist\n");
 break;
 
 case'F':case'f':
-
+if (b>-454)
  printf("C=%.2f""\nK=%.2f",(b-32)/1.8,((b-32)/1.8)+273.15);
+ else
+    printf("temperature doesnt exist\n");
  break;
 
  case'K':case'k':
-
+if(b>0)
  printf("C=%.2f""\nF=%.2f",(b-273.15),((b-273.15)*1.8)+32);
+ else
+    printf("temperature doesnt exist\n");
 break;
 
 default:
@@ -36,11 +46,9 @@ default:
     printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
     printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
  break;
-}
+}}
 
 
 return 0;
 }
-
-
 
