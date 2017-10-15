01d71257e566b736834af9728849ffb0003279a4
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-10-15 17:24:20 +0300

    Update лабораторная1.1.c

--- лабораторная1.1.c
+++ лабораторная1.1.c
@@ -4,51 +4,44 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-char a;
+
 float b;
- b=atof(argv[1]);
- a=argv[2];
+
  if(argc==1){
     printf("Enter number and first letter of temperature");
  }
  else
  if (argc==2){
+    b=atof(argv[1]);
     printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
     printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
     printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
  }
 else{
-switch(a){
-case'C':case'c':
-if (b>-273)
- printf("F=%.2f""\nK=%.2f",(b*1.8+32),b+273.15);
- else
-    printf("temperature doesnt exist\n");
-break;
+        if (strcmp(argv[2], "C")==0 || strcmp(argv[2], "c")==0){
 
-case'F':case'f':
-if (b>-454)
- printf("C=%.2f""\nK=%.2f",(b-32)/1.8,((b-32)/1.8)+273.15);
- else
-    printf("temperature doesnt exist\n");
- break;
+                if (b>-273)
+                printf("F=%.2f""\nK=%.2f",(b*1.8+32),b+273.15);}
+                else
+                printf("temperature doesnt exist\n");;
+        if (strcmp(argv[2], "F")==0 || strcmp(argv[2], "f")==0){
+                if (b>-454)
+                printf("C=%.2f""\nK=%.2f",(b-32)/1.8,((b-32)/1.8)+273.15);
+                else
+                printf("temperature doesnt exist\n");
+        }
 
- case'K':case'k':
-if(b>0)
- printf("C=%.2f""\nF=%.2f",(b-273.15),((b-273.15)*1.8)+32);
- else
-    printf("temperature doesnt exist\n");
-break;
-
-default:
-
-    printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
-    printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
-    printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
- break;
-}}
+        if (strcmp(argv[2], "K")==0 || strcmp(argv[2], "k")==0){
+            if(b>0)
+            printf("C=%.2f""\nF=%.2f",(b-273.15),((b-273.15)*1.8)+32);
+            else
+            printf("temperature doesnt exist\n");
+        }
+}
 
 
 return 0;
 }
+
+
 
