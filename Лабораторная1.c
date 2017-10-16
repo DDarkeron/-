34787f9aa17f8f898a95b93603100faf590a0b28
DDarkeron <31534102+DDarkeron@users.noreply.github.com>
2017-10-16 16:31:01 +0300

    Update Лабораторная1.c

--- Лабораторная1.c
+++ Лабораторная1.c
@@ -10,27 +10,31 @@ float b;
     printf("Enter number and first letter of temperature");
  }
  else
- if (argc==2){
+ if (argc==2)/*types results for everything, if there wasn't mentioned any temperature*/
+ {
     b=atof(argv[1]);
     printf("\nC=%.2f""\nF=%.2f""\nK=%.2f\n",b,((b*1.8)+32),(b+273.15));
     printf("\nF=%.2f""\nC=%.2f""\nK=%.2f\n",b,((b-32)/1.8),(((b-32)/1.8)+273.15));
     printf("\nK=%.2f""\nC=%.2f""\nF=%.2f\n",b,(b-273.15),((b-273.15)*1.8)+32);
  }
 else{
-        if (strcmp(argv[2], "C")==0 || strcmp(argv[2], "c")==0){
+        if (strcmp(argv[2], "C")==0 || strcmp(argv[2], "c")==0)/*types results, if Celsius was mentioned*/
+        {
 
                 if (b>-273)
                 printf("F=%.2f""\nK=%.2f",(b*1.8+32),b+273.15);}
                 else
                 printf("temperature doesnt exist\n");;
-        if (strcmp(argv[2], "F")==0 || strcmp(argv[2], "f")==0){
+        if (strcmp(argv[2], "F")==0 || strcmp(argv[2], "f")==0)/*types results, if Fahrenheit was mentioned*/
+       {
                 if (b>-454)
                 printf("C=%.2f""\nK=%.2f",(b-32)/1.8,((b-32)/1.8)+273.15);
                 else
                 printf("temperature doesnt exist\n");
         }
 
-        if (strcmp(argv[2], "K")==0 || strcmp(argv[2], "k")==0){
+        if (strcmp(argv[2], "K")==0 || strcmp(argv[2], "k")==0)/*types results, if Kelvin was mentioned*/
+        {
             if(b>0)
             printf("C=%.2f""\nF=%.2f",(b-273.15),((b-273.15)*1.8)+32);
             else
